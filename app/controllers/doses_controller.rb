9a07ed65737b6472c8083fa02723d662fd785a4e
Marina Caetano <marinarcaetano@yahoo.com.br>
2020-11-30 15:18:00 -0300

    controller cocktails

---
+++ app/controllers/doses_controller.rb
@@ -0,0 +1,34 @@
+class DosesController < ApplicationController
+  def new
+    @dose = Dose.new
+    @cocktail = Cocktail.find(params[:cocktail_id])
+    @ingredients = Ingredient.all
+  end
+
+  def create
+    @dose = Dose.new(dose_params)
+    @cocktail = Cocktail.find(params[:cocktail_id])
+    @dose.cocktail = @cocktail
+    @ingredient = Ingredient.find(params[:dose][:ingredient])
+    @dose.ingredient = @ingredient
+    if @dose.save
+      redirect_to cocktail_path(@cocktail)
+    else
+      render :new
+    end
+  end
+  
+  def destroy
+    @dose = Dose.find(params[:id])
+    @dose.destroy
+
+    # redirect_to doses_path
+  end
+
+  private
+
+  def dose_params
+    params.require(:dose).permit(:description, :ingredient_id)
+  end
+
+end
